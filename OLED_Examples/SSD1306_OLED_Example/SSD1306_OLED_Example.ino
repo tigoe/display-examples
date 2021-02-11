056436cde5675b8eca52b57497edae465e80f3de
Tom Igoe <tom.igoe@gmail.com>
2021-02-11 06:57:16 -0500

    Update SSD1306_OLED_Example.ino

--- OLED_Examples/SSD1306_OLED_Example/SSD1306_OLED_Example.ino
+++ OLED_Examples/SSD1306_OLED_Example/SSD1306_OLED_Example.ino
@@ -20,8 +20,12 @@
  
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
+// see https://learn.adafruit.com/adafruit-gfx-graphics-library/using-fonts
+// for a list of fonts available in the GFX lib
+// or custom fonts from http://oleddisplay.squix.ch/#/home
+#include<Fonts/FreeSans9pt7b.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET    0  // Reset pin for display (0 or -1 if no reset pin)
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -31,11 +35,13 @@ void setup() {
   // initialize serial and wait for serial monitor to open:
   Serial.begin(9600);
   while (!Serial);
-  // I2C address is 0x3C
+  // I2C address is 0x3C, or 3D for some 128x64 modules: 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println("Display setup failed");
     while (true);
   }
+    // set fonts botforh display:
+  display.setFont(&FreeSans9pt7b);
   Serial.println("Display is good to go");
 }
 
@@ -48,19 +54,19 @@ void loop() {
   */
   // clear the display:
   display.clearDisplay();
-  // set the text size to 2:
-  display.setTextSize(2);
+  // set the text size to 1:
+  display.setTextSize(1);
   // set the text color to white:
   display.setTextColor(SSD1306_WHITE);
   
   // move the cursor to 0,0:
-  display.setCursor(0, 0);
+  display.setCursor(0, 12);
   // print the seconds:
   display.print("secs:");
   display.print(millis() / 1000);
   
   // move the cursor down 20 pixels:
-  display.setCursor(0, 20);
+  display.setCursor(0, 30);
   // print a sensor reading:
   display.print("sensor:");
   display.print(sensorReading);
